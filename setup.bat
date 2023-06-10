dfc010365b992e4b60ae20c46257290a5951c276
みゃみゃ <115388782+myamya-wiki@users.noreply.github.com>
2023-06-10 15:32:03 +0000

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+echo モジュールのインストールを開始します...
+
+REM tkinterのインストール
+python -m pip install tkinter
+
+REM requestsのインストール
+python -m pip install requests
+
+echo モジュールのインストールが完了しました。
+pause
